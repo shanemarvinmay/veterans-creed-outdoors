1b3224201e14e1d8831011fccffadff92dfde263
shanemarvinmay <shanemarvinmay@gmail.com>
2022-05-30 16:57:36 -0700

    Add logo img

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Button from '@mui/material/Button';
 import Nav from './Nav';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import MainCard from './MainCard';
 
 const darkTheme = createTheme({
   palette: {
@@ -14,6 +15,7 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <Nav />
+      <MainCard />
       <Button variant="contained">Material Design in React</Button>
     </ThemeProvider>
   );
---
+++ src/MainCard.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function MainCard() {
+    console.log(CardMedia);
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="100%"
+          image="IMG_5087.png"
+          alt="Veteran's Creed Outdoors"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            What we do
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          We are a Veteran Organization with a faith in Christ, Our Nations Hero’s and the Outdoors, and our mission is to serve those who sacrifice for this country.  We are a Christian based non-profit (501C3 in process) that uses the outdoors as a tool to make Veteran’s, Police and Fireman feel appreciated, loved and wanted. We volunteer our time to take these brave men and women, and their families, out hunting and fishing around the country for free to thank them for their service, help them get away from the stress in their life, and help them be able to live in the moment. We are here for you as Veteran Brothers and Sisters, and as a Family that you can reach out to if you need someone to be there.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
