d72e47d85998dc4d100809aef90ff93b560ad5ac
shanemarvinmay <shanemarvinmay@gmail.com>
2022-06-20 17:37:30 -0700

    fixed image sizing for mobile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Grid } from '@mui/material';
 import MainCard from './MainCard';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
 const darkTheme = createTheme({
   palette: {
@@ -10,31 +13,30 @@ const darkTheme = createTheme({
   },
 });
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <Nav />
-      <MainCard />
+
+      <Grid container spacing={2}>
+        <Grid item xs={2} md={2}>
+        </Grid>
+        <Grid item xs={8} md={8}>
+          <MainCard />
+        </Grid>
+        <Grid item xs={2} md={2}>
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
